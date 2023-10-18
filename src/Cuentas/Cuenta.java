302723d5464b61a17531c1c411c00c07bf21f6fd
al064697 <al064697@uacam.mx>
2023-10-17 19:52:05 -0600

    se agregó setTotalCuenta

--- src/Cuentas/Cuenta.java
+++ src/Cuentas/Cuenta.java
@@ -3,9 +3,12 @@ package Cuentas;
 public class Cuenta {
     int id;
     String nombre;
+    int totalCuenta;
+
     Cuenta(int id, String nombre) {
         this.id = id;
         this.nombre = nombre;
     }
+    public void setTotalCuenta(int totalCuenta) {this.totalCuenta = totalCuenta}
     @Override public String toString() {return  id + nombre;}
 }
